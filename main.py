e171084dfd9f422af426f20752444f254b378408
Bormental <tyranosaurus@yandex.ru>
2022-12-10 18:53:38 +0300

    modified and added

--- main.py
+++ main.py
@@ -2,26 +2,46 @@
 # Пример:
 # - при d = 0.001, π = 3.141.    10^{-1} ≤ d ≤ 10^{-10}
 
+# my var
 # from math import pi
 # d = input("Введите точность отображения числа Пи, число 10^{-1} ≤ d ≤ 10^{-10} : ")
 # round_lenth = len(d.split('.')[1])
 # print('Число Пи с заданной точностью', d, 'равно', round(pi, round_lenth))
 
+# var1
+# from math import pi
+# d = input()
+# accur = len(d)
+# print(str(pi)[0:accur])
+
 # ________________________________________________________________________
 # 31. Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.
 
+# my var
 # num = int(input("Введите число: "))
 # list_simples = [1]
 # for i in range(2, num + 1):
-#     if num % i == 0:
-#         count = 1
+#     if not num % i:
 #         for j in range(2, (i // 2 + 1)):
-#             if i % j == 0:
-#                 count = 0
+#             if not i % j:
 #                 break
-#         if (count == 1):
+#         else:
 #             list_simples.append(i)
 # print(list_simples)
+
+# optimal var
+# number_set = set()
+# out_list = []
+# some_list = list(map(int, input().split()))
+# for ind in range(0, len(some_list)):
+#     if some_list[ind] not in number_set:
+#         number_set.add(some_list[ind])
+#         for ind1 in range(ind + 1, len(some_list)):
+#             if some_list[ind] == some_list[ind1]:
+#                 break
+#         else:
+#             out_list.append(some_list[ind])
+# print(out_list)
 
 # _____________________________________________________________________
 # 32. Задайте последовательность чисел. Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности.
